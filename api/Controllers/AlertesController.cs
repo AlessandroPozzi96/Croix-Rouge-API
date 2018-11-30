82612f1d5bc4b36824b61a59bd72f551177a7c2f
Gwynbleidd96 <alessandro.pozzi72@gmail.com>
2018-11-30 11:19:29 +0100

    Test de récupération d'un controller Alerte pour récupérer les infos dans Angular

--- api/Controllers/AlertesController.cs
+++ api/Controllers/AlertesController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using api.model;
 
 namespace api.Controllers
 {
@@ -13,19 +14,24 @@ namespace api.Controllers
     [ApiController]
     public class AlertesController : ControllerBase
     {
+        private Alerte[] alertes = new Alerte[] {
+            new Alerte(1, "Stock AB+ faible", "La croix rouge estime que le niveau de sang AB+ ne sera pas suffisant pour satisfaire la demande, nous vous demandons votre aide !"), 
+            new Alerte(2, "Stock O-", "Message a tous les donneurs universelles, suite aux récents attentats nous vous demandons de venir le plus vite possible donner votre sang pour sauver un maximum de personnes"), 
+            new Alerte(3, "Stock All", "Cher donneurs et cher donneuses, la croix rouge dispose de suffisamment de sang pour le moment, ne vous sentez pas obligué de venir ces temps ci")
+        };
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public ActionResult<IEnumerable<Alerte>> Get()
         {
             User.Claims.ToList().ForEach(claim => Console.WriteLine($"Claim : {claim.Type}: {claim.Value}"));
-            return new string[] { "value1", "value2" };
+            return this.alertes;
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        public ActionResult<Alerte> Get(int id)
         {
-            return "value";
+            return this.alertes[id];
         }
 
         // POST api/values
--- api/Model/Alerte.cs
+++ api/Model/Alerte.cs
@@ -2,11 +2,39 @@ using System;
 
 namespace api.model
 {
+    
     public class Alerte 
     {
+        private int id;
+        public int Id
+        {
+            get { return id;}
+            set { id = value;}
+        }
+        private string nom;
+        public string Nom
+        {
+            get { return nom;}
+            set { nom = value;}
+        }
+
+        private string contenu;
+        public string Contenu
+        {
+            get { return contenu;}
+            set { contenu = value;}
+        }
+        
+        public Alerte(int id, string nom, string contenu)
+        {
+            Id = id; 
+            Nom = nom; 
+            Contenu = contenu;
+        }
+        
         public Alerte ()
         {
-            
+
         }
     }
 }
