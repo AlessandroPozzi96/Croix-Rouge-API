a0ab882ff83c1c1e7f2ee3079f4cd8a679ba424c
Brahy sebastien <etu31950@henallux.be>
2018-12-03 14:30:28 +0100

    test commit

--- api/Model/AuthenticationRepository.cs
+++ api/Model/AuthenticationRepository.cs
@@ -9,6 +9,7 @@ namespace api.model
         {
             new User() {UserName= "janedoe", Email="jane@doe.com", Id=1, PassWord="123"}, 
             new User() {UserName= "johndoe", Email="john@doe.com", Id=2, PassWord="456"}
+            //test commit
         };
 
         public IEnumerable<User> GetUsers()
