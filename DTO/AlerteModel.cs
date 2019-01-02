9a731fe443009be7b8a32d183d1081e75e61adc4
Gwynbleidd96 <alessandro.pozzi72@gmail.com>
2019-01-02 10:26:36 +0100

    DTO : Alerte : Groupe sanguin plus requis

--- DTO/AlerteModel.cs
+++ DTO/AlerteModel.cs
@@ -13,7 +13,6 @@ namespace CroixRouge.DTO
         [StringLength(500, MinimumLength=2)]
         [Required]
         public string Contenu { get; set; }
-        [Required]
         [StringLength(3, MinimumLength=2)]
         public string FkGroupesanguin { get; set; }
         public byte[] Rv { get; set; }
